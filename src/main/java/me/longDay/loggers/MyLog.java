85ed9f69df1e8098cf6def818aeac63a841cb99d
longDay <rosyevening@qq.com>
2022-12-25 18:42:35 +0800

    2022.12.25

--- src/main/java/me/longDay/loggers/MyLog.java
+++ src/main/java/me/longDay/loggers/MyLog.java
@@ -14,6 +14,7 @@ public class MyLog {
 
     public static void main(String[] args) {
         log.debug("Hello world 测试debug日志");
+        log.debug("Hello world 测试debug日志");
         log.info("Hello world 测试info日志");
         log.warn("Hello world 测试warn日志");
         log.error("Hello world 测试error日志");
